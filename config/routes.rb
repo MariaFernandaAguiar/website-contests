b99cfbadb3351bd86362948056a1694bcdeab5b5
maria <abreuaguiar07@gmail.com>
2023-03-03 09:09:52 -0400

    initial commit and config device

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :admins
+
+  root to: 'site/welcome#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
