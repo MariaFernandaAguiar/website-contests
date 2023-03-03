31d8c55e260c623c0d01b9a0e654b836410b8c5e
maria <abreuaguiar07@gmail.com>
2023-03-03 14:45:33 -0400

    porting the SBAdmin theme to Admin

---
+++ app/assets/javascript/admins_backoffice.js
@@ -0,0 +1,4 @@
+//= relative jquery/dist/jquery
+//= relative bootstrap/js/bootstrap.bundle.min
+//= relative jquery-easing/dist/jquery
+//= relative sb-admin-2
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,9 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#app/assets 
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admin_backoffice.css )
+
+ #lib/assets
+ Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+ 
